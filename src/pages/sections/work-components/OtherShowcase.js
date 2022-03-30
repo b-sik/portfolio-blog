c4aef56821194d0d461782e78dabb5c97e18f823
Brian Siklinski <brian.siklinski@19ideas.com>
2022-03-30 16:55:59 -0400

    strong tags

--- src/pages/sections/work-components/OtherShowcase.js
+++ src/pages/sections/work-components/OtherShowcase.js
@@ -11,11 +11,23 @@ import Row from "react-bootstrap/Row"
 const PluginShowcase = () => {
   return (
     <Container className="d-flex flex-column h-100 flex-grow-1">
-      <Col xs={{ span: 10, offset: 1 }} className="flex-grow-1 d-flex justify-content-center align-items-center">
+      <Col
+        xs={{ span: 10, offset: 1 }}
+        className="flex-grow-1 d-flex justify-content-center align-items-center"
+      >
         <ListGroup>
-          <ListGroup.Item variant="light">&rarr;&nbsp;Take Your Meds Bot | ADHD-inspired Twitter bot powered by AWS Lambda functions and written with Python.</ListGroup.Item>
-          <ListGroup.Item variant="light">&rarr;&nbsp;WP Zip | Bash script delivered via Composer package to prepare a zip archive for a WordPress plugin.</ListGroup.Item>
-          <ListGroup.Item variant="light">&rarr;&nbsp;This Website | Fully static site made with Gatsby, React, and Bootstrap.</ListGroup.Item>
+          <ListGroup.Item variant="light">
+            &rarr;&nbsp;<strong>Take Your Meds Bot</strong> | ADHD-inspired
+            Twitter bot powered by AWS Lambda functions and written with Python.
+          </ListGroup.Item>
+          <ListGroup.Item variant="light">
+            &rarr;&nbsp;<strong>WP Zip</strong> | Bash script delivered via
+            Composer package to prepare a zip archive for a WordPress plugin.
+          </ListGroup.Item>
+          <ListGroup.Item variant="light">
+            &rarr;&nbsp;<strong>This Website</strong> | Fully static site made
+            with Gatsby, React, and Bootstrap.
+          </ListGroup.Item>
         </ListGroup>
       </Col>
     </Container>
