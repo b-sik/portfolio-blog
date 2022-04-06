fa4c09621425d72dcb46bd1fad480f41b08182cb
Brian Siklinski <bszyk.codes@gmail.com>
2022-04-05 21:12:21 -0400

    shared showcase component

---
+++ src/pages/sections/work-components/showcase.js
@@ -0,0 +1,73 @@
+import React from "react"
+import { shuffleArray } from "../../../helpers"
+
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
+import ListGroup from "react-bootstrap/ListGroup"
+import Row from "react-bootstrap/Row"
+
+const Showcase = ({ description, items }) => {
+  const variants = shuffleArray([
+    "primary",
+    "secondary",
+    "success",
+    "info",
+    "warning",
+    "danger",
+  ])
+
+  return (
+    <Container className="d-flex flex-column h-100 flex-grow-1">
+      <Row className="flex-grow-1">
+        <Col
+          xs={12}
+          className="flex-grow-1 d-flex justify-content-center align-items-end mb-4 text-center"
+        >
+          <p className="text-light">{description}</p>
+        </Col>
+      </Row>
+      <Row className="flex-grow-1 justify-content-around">
+        <Col
+          xs={12}
+          className="flex-grow-1 d-flex justify-content-center align-items-start"
+        >
+          <ListGroup>
+            {Object.values(items).map((item, i) => {
+              return (
+                <ListGroup.Item variant={variants[i]} className="text-dark">
+                  <Container>
+                    <Row>
+                      <Col xs={9}>
+                        &rarr;&nbsp;<strong>{item.name}</strong> |{" "}
+                        {item.description}
+                      </Col>
+                      <Col
+                        xs={3}
+                        className="d-flex flex-column align-items-center justify-content-center"
+                      >
+                        {item.buttons.map((button, i) => (
+                          <Button
+                            href={button[1]}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            variant="outline-dark"
+                            className={`w-100 ${i > 0 ? "mt-2" : ""}`}
+                          >
+                            {button[0]}
+                          </Button>
+                        ))}
+                      </Col>
+                    </Row>
+                  </Container>
+                </ListGroup.Item>
+              )
+            })}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Showcase
--- src/pages/sections/work.js
+++ src/pages/sections/work.js
@@ -5,9 +5,7 @@ import Container from "react-bootstrap/Container"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 
-import OtherShowcase from "./work-components/OtherShowcase"
-import PluginShowcase from "./work-components/PluginShowcase"
-import ThemeShowcase from "./work-components/ThemeShowcase"
+import Showcase from "./work-components/showcase"
 
 const Work = () => {
   const [workItem, setWorkItems] = useState("work-item-0")
@@ -39,14 +37,86 @@ const Work = () => {
     })
   }
 
-  const workShowcaseComponent = workItem => {
+  const workItemData = workItem => {
     switch (workItem) {
       case "work-item-0":
-        return <ThemeShowcase />
+        return {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          items: {
+            breathtobody: {
+              name: "Breath To Body",
+              description:
+                "Rehabbed a bloated WordPress site with a custom theme and easy-to-update Content Management System.",
+              buttons: [["visit site", "http://breathtobody.com"]],
+            },
+            westferry: {
+              name: "West Ferry",
+              description:
+                "Created a central hub for this band to show-off their work, run a blog, and archive their shows and albums.",
+              buttons: [["visit site", "https://westferrymusic.com"]],
+            },
+          },
+        }
       case "work-item-1":
-        return <PluginShowcase />
+        return {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          items: {
+            dynamiccontent: {
+              name: "Dynamic Content",
+              description:
+                "A plugin for the WordPress editor which allows core blocks to become dynamic, updating automatically on the frontend if a database change is detected.",
+              buttons: [
+                [
+                  "wordpress",
+                  "http://breathtobody.comhttps://wordpress.org/plugins/dynamic-block-content/",
+                ],
+                ["github", "https://github.com/bszyk/dynamic-block-content"],
+              ],
+            },
+            advacc: {
+              name: "Advanced Access for LearnDash",
+              description:
+                "A plugin for LearnDash which adds functionality to control access to courses, including creating a digital WooCommerce access pass.",
+              buttons: [["github", "http://github.com/bszyk/adv-acc-ld-wc"]],
+            },
+            sso: {
+              name: "Custom SSO for WordPress",
+              description:
+                "A custom SSO for WordPress utilizing JWT Auth tokens.",
+              buttons: [["github", "https://wordpress.org/plugins/bszyk-sso"]],
+            },
+          },
+        }
       case "work-item-2":
-        return <OtherShowcase />
+        return {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          items: {
+            dynamiccontent: {
+              name: "Take Your Meds Bot",
+              description:
+                "ADHD-inspired Twitter bot powered by AWS Lambda functions and written with Python.",
+              buttons: [
+                ["twitter", "https://twitter.com/takeyourmedsbot"],
+                ["github", "https://github.com/bszyk/twitter-bots"],
+              ],
+            },
+            advacc: {
+              name: "Zip WP",
+              description:
+                "Bash script deliveredvia Composer package to prepare a zip archive for a WordPress plugin.",
+              buttons: [["github", "http://github.com/bszyk/bszyk-zip-wp"]],
+            },
+            sso: {
+              name: "This Website",
+              description:
+                "Fully static custom site made with Gatsby, React, and Bootstrap.",
+              buttons: [["github", "https://github.com/bszyk/portfolio-blog"]],
+            },
+          },
+        }
       default:
         break
     }
@@ -78,7 +148,7 @@ const Work = () => {
             xs={7}
             className="d-flex justify-content-center align-items-center"
           >
-            {workShowcaseComponent(workItem)}
+            <Showcase {...workItemData(workItem)} />
           </Col>
         </Row>
       </Container>
