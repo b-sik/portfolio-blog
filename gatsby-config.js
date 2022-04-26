f215257a2452fe734df5a5d693ee2040e5400203
Brian Siklinski <bszyk.codes@gmail.com>
2022-04-25 22:27:20 -0400

    social card plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,6 +128,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    'gatsby-plugin-social-cards',
   ],
 }
