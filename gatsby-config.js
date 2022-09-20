67ca8dcaf612fe0d8796c7d2f9c045a240a1eb03
Brian Siklinski <bszyk.codes@gmail.com>
2022-09-20 15:41:36 -0400

    update gtag config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,31 +12,22 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-SQVHCCLBJH", // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
+        trackingId: "G-SQVHCCLBJH",
         gtagConfig: {
           anonymize_ip: true,
           cookie_expires: 0,
         },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
+          head: true,
           respectDNT: true,
         },
       },
     },
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
