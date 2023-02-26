89a88018f9f30aa521276261b0ee8f392d3b5a81
Brian Siklinski <bszyk.codes@gmail.com>
2023-02-26 00:44:49 -0500

    add client ip to data response

--- src/api/visitorLog.js
+++ src/api/visitorLog.js
@@ -1,5 +1,6 @@
-const axios = require("axios")
-const crypto = require("crypto")
+import axios from "axios"
+import crypto from "crypto"
+import { publicIpv4 } from "public-ip"
 
 export default async function visitorLogPOST(req, res) {
   const url = "http://68.183.113.10:3000/visitor-log"
@@ -9,9 +10,11 @@ export default async function visitorLogPOST(req, res) {
     // Authorization: `Bearer ${process.env.CLIENT_TOKEN}`,
   }
 
+  const clientIp = await getClientIp()
+
   const data = {
     uid: crypto.randomBytes(16).toString("hex"),
-    ip_addr: "gatsby-test-axios",
+    ip_addr: clientIp,
     timestamp: new Date(),
   }
 
@@ -19,15 +22,23 @@ export default async function visitorLogPOST(req, res) {
     const result = await axios
       .post(url, data, {
         headers,
+        timeout: 3000,
       })
       .then(postRes => {
         console.log("Axios POST: " + postRes.status)
         return res.status(postRes.status).json({
           status: postRes.status,
           message: postRes.statusText,
+          data,
         })
       })
   } catch (error) {
     res.status(500).send(error)
   }
 }
+
+const getClientIp = async () =>
+  await publicIpv4({
+    fallbackUrls: ["https://ifconfig.co/ip"],
+    timeout: 3000,
+  })
